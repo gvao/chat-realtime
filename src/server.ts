62320a55077dafcee05efa8d66f8017b58a504c1
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-21 12:09:13 -0300

    created server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,33 @@
-type person = {
-    name: string,
-    age: number,
+import http from "http";
+import WebSocket from "ws";
+
+const PORT = 5501
+
+const server = http.createServer()
+const wss = new WebSocket.Server({ server })
+
+try {
+    wss.on("connection", ws => {
+        ws.on('message', (data: string) => {
+            const parsedData = JSON.parse(data)
+            
+            console.log(`on message`, parsedData)
+            
+            wss.clients
+                .forEach(client => {
+                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                        
+                        client.send(JSON.stringify(parsedData))
+                    }
+                })
+
+        })
+    })
+
+} catch (err) {
+    console.error(err)
 }
 
-const me : person = {
-    age: 19,
-    name: `Yuri `
-}
+server.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}: http://localhost:${PORT}`)
+})
